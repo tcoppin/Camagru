717051a9ba65a186ccaad4982859c2d0ecb2fd43
Tanguy COPPIN <tcoppin@e1r13p14.42.fr>
2016-05-03 23:15:39 +0200

    modif toto.php

--- app/toto.php
+++ app/toto.php
@@ -1,45 +1,37 @@
 <video></video>
 <button type="submit" name="button" value="sendPassForget" id="takePicture" class="ca_collapse">Prendre une photo</button>
-<img src="" />
-<canvas style="display:none;"></canvas>
+<canvas width="640" height="480"></canvas>
 <script type="text/javascript">
-	navigator.getUserMedia = ( navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
+	navigator.getUserMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
 
-if (navigator.getUserMedia) {
-   navigator.getUserMedia (
+	if (navigator.getUserMedia) {
+		navigator.getUserMedia (
 
-      // constraints
-      {
-         video: true,
-         audio: false
-      },
+			{
+				video: true,
+				audio: false
+			},
 
-      // successCallback
-      function(localMediaStream) {
-        var video = document.querySelector('video');
-        video.src = window.URL.createObjectURL(localMediaStream);
-        // Do something with the video here, e.g. video.play()
-        video.play();
-        function takePicture() {
-			if (localMediaStream) {
-				var canvas = document.createElement('canvas');
-			  	var ctx = canvas.getContext && canvas.getContext('2d');
-			    ctx.drawImage(video, 0, 0);
-			    document.querySelector('img').src = canvas.toDataURL('image/webp');
+			function(localMediaStream) {
+				var video = document.querySelector('video');
+				video.src = window.URL.createObjectURL(localMediaStream);
+				video.play();
+				function takePicture() {
+					if (localMediaStream) {
+						var canvas = document.querySelector('canvas');
+						var ctx = canvas.getContext('2d');
+						ctx.drawImage(video, 0, 0, 640, 480);
+					}
+				}
+				document.getElementById('takePicture').addEventListener('click', takePicture, false);
+			},
+
+			// errorCallback
+			function(err) {
+				console.log("The following error occured: " + err);
 			}
-		}
-		document.getElementById('takePicture').addEventListener('click', takePicture, false);
-      },
-
-      // errorCallback
-      function(err) {
-         console.log("The following error occured: " + err);
-      }
-   );
-} else {
-   console.log("getUserMedia not supported");
-}
+		);
+	} else {
+		console.log("getUserMedia not supported");
+	}
 </script>
